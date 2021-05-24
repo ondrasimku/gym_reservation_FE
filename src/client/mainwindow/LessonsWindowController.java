539fc3ad30e58136dc43ac46f387a34fc0623295
BuildTools <unconfigured@null.spigotmc.org>
2021-05-24 22:10:58 +0200

    Working delete

--- src/client/mainwindow/LessonsWindowController.java
+++ src/client/mainwindow/LessonsWindowController.java
@@ -32,7 +32,7 @@ public class LessonsWindowController {
     private ObservableList<Lesson> list;
 
     @FXML
-    private Button btnBack, btnBookIn;
+    private Button btnBack, btnBookIn, btnDelete;
     @FXML
     private TableView<Lesson> tableViewLessons;
     @FXML
@@ -47,7 +47,7 @@ public class LessonsWindowController {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation Dialog");
         alert.setHeaderText("Book in lesson");
-        alert.setContentText("Are sure you want to to book this lesson?");
+        alert.setContentText("Are sure you want to book this lesson?");
         Optional<ButtonType> result = alert.showAndWait();
         if(result.get() == ButtonType.OK) {
 
@@ -66,7 +66,7 @@ public class LessonsWindowController {
                 String serverResponse;
                 serverResponse = (String)serverInput.readObject();
                 if(serverResponse.equals("bookin:failed")) {
-                    Alert(Alert.AlertType.ERROR, "Book in status" ,"Book in failed!","Something went!");
+                    Alert(Alert.AlertType.ERROR, "Book in status" ,"Book in failed!","Something went wrong!");
                 } else {
                     Alert(Alert.AlertType.INFORMATION, "Book in status" ,"Book in successfull!","You got booked in!");
                     try {
@@ -95,6 +95,58 @@ public class LessonsWindowController {
         MainWindowController mainController = fxmlLoader.getController();
         mainController.init(primaryStage, clientSocket, serverInput, clientOutput, SOCKET_HOST, SOCKET_PORT, user);
         this.primaryStage.setScene(new Scene(mainScene, 600, 400));
+    }
+
+    public void btnDeleteHandler(ActionEvent e) {
+        Lesson selectedLesson = tableViewLessons.getSelectionModel().getSelectedItem();
+        if(selectedLesson == null) {
+            Alert(Alert.AlertType.ERROR, "Delete status" ,"Delete failed!","Select lesson!");
+            return;
+        }
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Delete lesson");
+        alert.setContentText("Are sure you want to delete this lesson?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK) {
+
+            String command = "delete:" + selectedLesson.getID() + ":" + this.user.getId_user()
+                    + ":" + this.user.getUsername() + ":" + this.user.getPassword();
+            System.out.println("Sending to server -->\"" + command + "\"");
+            try {
+                clientOutput.writeObject(command);
+                clientOutput.flush();
+            } catch (IOException ioException) {
+                System.err.println("Error sending delete info, server might be down");
+                System.err.println(ioException.getMessage());
+            }
+
+            try {
+                String serverResponse;
+                serverResponse = (String)serverInput.readObject();
+                if(serverResponse.equals("delete:failed")) {
+                    Alert(Alert.AlertType.ERROR, "Delete status" ,"Delete failed!","Something went wrong!");
+                } else {
+                    Alert(Alert.AlertType.INFORMATION, "Delete status" ,"Delete successfull!","Lesson deleted!");
+                    try {
+                        this.user = (User)serverInput.readObject();
+                        this.list.removeAll();
+                        this.list = getLessons();
+                        this.tableViewLessons.setItems(this.list);
+                    } catch (ClassNotFoundException classNotFoundException) {
+                        Alert(Alert.AlertType.ERROR, "Delete status" ,"Delete failed!","Error fetching user info!");
+                        System.err.println("Invalid class input exception");
+                        classNotFoundException.printStackTrace();
+                    }
+                }
+            } catch (IOException ioException) {
+                System.err.println("Lost connection!");
+                reconnect();
+            } catch (ClassNotFoundException classNotFoundException) {
+                System.err.println("This should not happen!");
+                classNotFoundException.printStackTrace();
+            }
+        }
     }
 
     private ObservableList<Lesson> getLessons() {
@@ -143,6 +195,11 @@ public class LessonsWindowController {
         this.SOCKET_HOST = SOCKET_HOST;
         this.SOCKET_PORT = SOCKET_PORT;
         this.user = user;
+        if(this.user.getIs_instructor() == 1) {
+            btnDelete.setVisible(true);
+        } else {
+            btnDelete.setVisible(false);
+        }
         this.clnDate.setCellValueFactory(new PropertyValueFactory<Lesson, String>("date"));
         this.clnName.setCellValueFactory(new PropertyValueFactory<Lesson, String>("name"));
         this.clnText.setCellValueFactory(new PropertyValueFactory<Lesson, String>("text"));
